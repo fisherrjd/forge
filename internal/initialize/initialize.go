0fc5e81873148ff38a5d9947b33aaf0bb2e5bfde
jade fisher <fisherrjd@gmail.com>
2025-12-07 17:49:55 -0700

    begin defining toml values

--- internal/initialize/initialize.go
+++ internal/initialize/initialize.go
@@ -1,9 +1,36 @@
 package initialize
 
 import (
-    "fmt"    
+    "bufio"
+    "fmt"
+    "os"
+    "path/filepath"
+    "strings"
+
+    "github.com/pelletier/go-toml/v2"
 )
 
+type Config struct {
+    Project      ProjectInfo              `toml:"project"`
+    Build        BuildConfig              `toml:"build"`
+    Dependencies map[string][]Dependency  `toml:"dependencies"`
+}
+
+type ProjectInfo struct {
+    Group    string `toml:"group"`
+    Artifact string `toml:"artifact"`
+    Version  string `toml:"version"`
+}
+
+type BuildConfig struct {
+    JavaVersion string `toml:"java-version"`
+    Encoding    string `toml:"encoding"`
+}
+
+type Dependency struct {
+    Name    string `toml:"name"`
+    Version string `toml:"version"`
+}
 
 func Run(path string, useDefaults bool) error{
 	return generateToml(path, useDefaults)
